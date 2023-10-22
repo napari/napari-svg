2542e949981872867c46f35220c56abfeb7207b3
Grzegorz Bokota <bokota+github@gmail.com>
2023-10-22 18:32:02 +0200

    Remove napari-plugin-manager from deps, remove setup.py (#35)
    * remove napari-plugin-manager from deps, remove setup.py
    
    * drop python 3.7
    
    * update tox

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import setup
-
-setup(
-    use_scm_version={"write_to": "napari_svg/_version.py"},
-    setup_requires=["setuptools_scm"],
-)
