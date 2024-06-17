Bug fix: specify rotation in degrees

napari_svg/layer_to_xml.py
@@ -63,9 +63,9 @@ def layer_transforms_to_xml_string(meta):
     """
     scale = meta['scale'][::-1]
     translate = meta['translate'][::-1]
-    rotate = -np.arctan2(meta['rotate'][0][1], meta['rotate'][1][1])
     skewx = meta['shear'][0]
     # skewy = meta['shear'][0]
+    rotate = np.degrees(np.arctan2(meta['rotate'][0][1], meta['rotate'][1][1]))
     # matrix elements after converting row-column to y, x, first
     # flipping the rows and then the first two columns of the matrix:
     # a c e   ->   b d f   ->   d b f